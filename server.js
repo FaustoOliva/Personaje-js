cf1edb6adea7aa4d6614edced18a94e74aac4800
FaustoOliva <fausto.calavera@gmail.com>
2022-06-20 14:39:41 -0300

    Swagger

--- server.js
+++ server.js
@@ -3,17 +3,21 @@ import cors from "cors";
 import PersonajeRouter from "./src/controllers/PersonajeController.js";
 import SerieRouter from "./src/controllers/SerieController.js";
 import OuthRouter from "./src/controllers/OuthController.js";
+import swaggerUI from 'swagger-ui-express';
+import swaggerJsDoc from 'swagger-jsdoc';
 
 const app = express();
 const port = 5000;
 
 app.use(cors());
 app.use(express.json());
+const specs = swaggerJsDoc(options);
 
 app.use("/personaje", PersonajeRouter);
 app.use("/serie", SerieRouter);
 app.use("/auth", OuthRouter);
-  
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
